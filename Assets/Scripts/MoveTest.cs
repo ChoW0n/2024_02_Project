0c43f69f15e44d6785bcb4c70b1d6416a38bab87
ChoW0n <163250615+ChoW0n@users.noreply.github.com>
2024-09-04 13:45:58 +0900

    Update MoveTest.cs
    Move 변수 추가

--- Assets/Scripts/MoveTest.cs
+++ Assets/Scripts/MoveTest.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveTest : MonoBehaviour
 {
+    public class MoveTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
